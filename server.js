a1134038eb02b5b880141d6cdf8e999d5f8e8426
saiurakrishna <34538779+saiurakrishna@users.noreply.github.com>
2023-07-31 02:28:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('HELLO DEVOPS ENGINEERS FROM SRISH TECH.....');
+  res.send('HELLO FROM GITOPS and ARGOCD');
 });
 
 app.listen(PORT, HOST, () => {
