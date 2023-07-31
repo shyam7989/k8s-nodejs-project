a59a599eebb3eb4d263d39ab616103a6208996c3
saiurakrishna <34538779+saiurakrishna@users.noreply.github.com>
2023-07-31 08:52:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('HELLO WORLD FROM K8S AUTOMATION..................!!!!!!!!');
+  res.send('HELLO WORLD FROM K8S AUTOMATION BY USING GITOPS && ARGOCD..................!!!!!!!!');
 });
 
 app.listen(PORT, HOST, () => {
